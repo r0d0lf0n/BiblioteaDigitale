51ab90b935355d7d2e4b1fc65b3ba4f9c20239ff
Rodolfo Figueroa Mayo <rodolfo.figueroamayo@nttdata.com>
2024-07-29 10:53:23 +0200

    use the current .csv to persist some book (1000) in the DB by default.

--- src/bibliotecaDigitale/CatalogMain.java
+++ src/bibliotecaDigitale/CatalogMain.java
@@ -18,52 +18,27 @@ import com.j256.ormlite.support.ConnectionSource;
 import com.j256.ormlite.table.TableUtils;
 import com.opencsv.CSVReader;
 import controllers.views.CatalogController;
+import database.Book;
 import database.DatabaseConfig;
 import database.User;
 import views.CatalogView;
 
 public class CatalogMain {
-//	DatabaseConfig config;
+	DatabaseConfig config;
 	ConnectionSource connectionSource;
 	CatalogView catalogView;
+	Dao<Book, String> bookDao = null;
 
 	public CatalogMain() {
 		ShowView();
-//		config = new DatabaseConfig();
-//		connectionSource = config.getdbConnection();
-//		System.out.println(connectionSource);
-		
-		
-
-	 	
-//        // create an instance of Account
-//		Dao<User, String> userDao = config.getUserDao();
-//        User user = new User();
-//        user.setName("Jim");
-//        user.setSurname("Coakley");
-//        user.setPhone(null);
-//        user.setRole(0);
-//        user.setAddress("avenue 45");
-        
-
-//        // persist the account object to the database
-//        try {
-////			userDao.create(user);
-//	        // retrieve the account from the database by its id field (name)
-////	        User userTwo = userDao.queryForId(null)
-////	        System.out.println("User: " + userTwo.getName());
-//	        // close the connection source
-////	        connectionSource.close();
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
 	}
 
 	public void ShowView() {
-		List<String[]> book_catalog = new ArrayList<String[]>();
+		List<Book> book_catalog = null;
+		config = DatabaseConfig.getInstance();
+		bookDao = config.getBookDao();
 		try {
-			book_catalog = readBookCatalog();
+			book_catalog = bookDao.queryForAll();
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -71,22 +46,6 @@ public class CatalogMain {
 	    catalogView = new CatalogView();
 	    catalogView.setTitle("Books Catalog");
 		CatalogController catalogController = new CatalogController(catalogView, book_catalog);
-	}
-	
-	public List<String[]> readBookCatalog() throws Exception {
-		String filepath = "/assets/book_catalog.csv";
-		URI resourcePath = this.getClass().getResource(filepath).toURI();
-	    Path path = Paths.get(resourcePath);
-	    return readAllLines(path);
-	}
-		
-	public List<String[]> readAllLines(Path filePath) throws Exception {
-//		System.out.println(filePath);
-	    try (Reader reader = Files.newBufferedReader(filePath)) {
-	        try (CSVReader csvReader = new CSVReader(reader)) {
-	            return csvReader.readAll();
-	        }
-	    }
 	}
 	
 	public CatalogView getView() {
--- src/controllers/views/CatalogController.java
+++ src/controllers/views/CatalogController.java
@@ -5,16 +5,17 @@ import java.util.List;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
+import database.Book;
 import views.CatalogView;
 
 public class CatalogController {
 
-	private List<String[]> book_catalog;
+	private List<Book> book_catalog;
 	private CatalogView catalogView;
 	private DefaultTableModel model;
 	private JTable catalogTable;
 
-	public CatalogController(CatalogView view, List<String[]> catalog) {
+	public CatalogController(CatalogView view, List<Book> catalog) {
 		catalogView = view;
 		this.book_catalog = catalog;
 		view.setVisible(true);
@@ -22,14 +23,13 @@ public class CatalogController {
 	}
 
 	public void InitialiazeTable() {
-		Object[] columns = { "Author", "Year" };
+		Object[] columns = { "id", "Author", "Year" };
 		model = new DefaultTableModel();
 		model.setColumnIdentifiers(columns);
-//		model.addRow(new Object[] { "John Wick", "2022" });
-//		model.addRow(new Object[] { "John Wick1", "2023" });
-		for (String[] book : book_catalog) { 
-//			System.out.println(book[1]);
-			model.addRow(new Object[] {book[0], book[1]});
+		
+		for(int i = 1; i < book_catalog.size(); i++) {
+//			System.out.println(book_catalog.get(i)[0]);
+				model.addRow(new Object[] {i, book_catalog.get(i).getAuthor(), book_catalog.get(i).getYear()});
 		}
 
 		catalogTable = catalogView.getCatalogTable();
